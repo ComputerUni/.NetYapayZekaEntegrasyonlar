af79734598ec138d2cc9a648e44fe33a7afdc102
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-07-10 17:34:17 +0300

    rapid api

---
+++ NetCoreAI/NetCoreAI.Project3_RapidApi/Program.cs
@@ -0,0 +1,30 @@
+﻿using NetCoreAI.Project3_RapidApi.ViewModels;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+
+
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "c463c8364dmsh488787b2fde641dp1da61djsn746529f67d51" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    foreach(var series in apiSeriesViewModels)
+    {
+        Console.WriteLine(series.rank + "-" + series.title + "-Film Puanı: " + series.rating + "-Yapım Yılı: " + series.year);
+    }
+}
+
+Console.ReadLine();
